b20dab0debed7500a74940bb72b867d4df3a5b7b
divyamk99 <130544679+divyamk99@users.noreply.github.com>
2023-09-13 19:09:21 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,8 @@
 import streamlit
+import pandas
+import requests
+import snowflake.connector
+import urllib.error import URLError
 streamlit.title('My parents New Healthy Dinner')
 streamlit.header('Breakfast')
 streamlit.text('🥣 Omega 3 & Blueberry')
